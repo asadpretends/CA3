d70f5c454d8eea7f16612c08df9ac26f92a6a71d
asadpretends <i190522@nu.edu.pk>
2023-09-19 11:06:02 +0500

    Docker file

--- calculator.py
+++ calculator.py
@@ -1,8 +1,17 @@
+from flask import Flask
+app= Flask(__name__)
+
+@app.route('/calculator/add')
 def add(x,y):
     return x+y
 
+@app.route('/calculator/multiply')
 def multiply(x,y):
     return x*y
 # Main calculator loop
 add(1,1)
 multiply(2,2)
+
+
+
+
